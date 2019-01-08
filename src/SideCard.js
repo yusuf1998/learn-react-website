ff7f97dc17d84f39ec85379d34e8c45f661ce1b0
yusuf1998 <yusufhabibi1998@gmail.com>
2019-01-08 10:34:51 +0700

    fix some bugs

--- src/SideCard.js
+++ src/SideCard.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import MyImage from "./my-image.jpg";
 import {
   Button,
@@ -10,33 +10,36 @@ import {
   CardText
 } from "reactstrap";
 
-const SideCard = () => (
-  <Fragment>
-    <Card>
-      <CardImg top width="100%" src={MyImage} alt="banner" />
-      <CardBody>
-        <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">
-          Yusuf Habibi
-        </CardTitle>
-        <CardSubtitle
-          className="text-secondary mb-3 font-weight-light text-uppercase"
-          style={{ fontSize: "0.8rem" }}
-        >
-          Web Developer
-        </CardSubtitle>
-        <CardText
-          className="text-secondary mb-4"
-          style={{ fontSize: "0.75rem" }}
-        >
-          Full-stack web developer learning new hacks one day at a time. Web
-          technology enthusiast.
-        </CardText>
-        <Button color="success" className="font-weight-bold">
-          View Profile
-        </Button>
-      </CardBody>
-    </Card>
-  </Fragment>
-);
-
+class SideCard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Card>
+          <CardImg top width="100%" src={MyImage} alt="banner" />
+          <CardBody>
+            <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">
+              Yusuf Habibi
+            </CardTitle>
+            <CardSubtitle
+              className="text-secondary mb-3 font-weight-light text-uppercase"
+              style={{ fontSize: "0.8rem" }}
+            >
+              Web Developer
+            </CardSubtitle>
+            <CardText
+              className="text-secondary mb-4"
+              style={{ fontSize: "0.75rem" }}
+            >
+              Full-stack web developer learning new hacks one day at a time. Web
+              technology enthusiast.
+            </CardText>
+            <Button color="success" className="font-weight-bold">
+              View Profile
+            </Button>
+          </CardBody>
+        </Card>
+      </Fragment>
+    );
+  }
+}
 export default SideCard;
