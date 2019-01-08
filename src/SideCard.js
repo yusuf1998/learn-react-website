5f49f25d97e9bed69e328b5b1cee5a24218bf8fc
yusuf1998 <yusufhabibi1998@gmail.com>
2019-01-08 10:42:14 +0700

    set content to state in SideCard.js

--- src/SideCard.js
+++ src/SideCard.js
@@ -11,6 +11,12 @@ import {
 } from "reactstrap";
 
 class SideCard extends Component {
+  state = {
+    namePeople: `Yusuf Habibi`,
+    position: `Web Developer`,
+    aboutPeople: `Full-stack web developer learning new hacks one day at a time. Web
+              technology enthusiast.`
+  };
   render() {
     return (
       <Fragment>
@@ -18,20 +24,19 @@ class SideCard extends Component {
           <CardImg top width="100%" src={MyImage} alt="banner" />
           <CardBody>
             <CardTitle className="h3 mb-2 pt-2 font-weight-bold text-secondary">
-              Yusuf Habibi
+              {this.state.namePeople}
             </CardTitle>
             <CardSubtitle
               className="text-secondary mb-3 font-weight-light text-uppercase"
               style={{ fontSize: "0.8rem" }}
             >
-              Web Developer
+              {this.state.position}
             </CardSubtitle>
             <CardText
               className="text-secondary mb-4"
               style={{ fontSize: "0.75rem" }}
             >
-              Full-stack web developer learning new hacks one day at a time. Web
-              technology enthusiast.
+              {this.state.aboutPeople}
             </CardText>
             <Button color="success" className="font-weight-bold">
               View Profile
