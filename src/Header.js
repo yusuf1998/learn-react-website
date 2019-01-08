1156ab79fe70a8489f30912c80f1500586b312cd
yusuf1998 <yusufhabibi1998@gmail.com>
2019-01-08 09:12:18 +0700

    First Commit

---
+++ src/Header.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Input,
+  Button,
+  Navbar,
+  Nav,
+  NavLink,
+  NavItem
+} from "reactstrap";
+
+const Header = () => (
+  <header>
+    <Navbar
+      fixed="top"
+      color="light"
+      light
+      expand="xs"
+      className="border-bottom border-gray bg-white"
+      style={{ height: 80 }}
+    >
+      <Container>
+        <Row noGutters className="position-relative w-100 align-items-center">
+          <Col className="d-none d-lg-flex justify-content-start">
+            <Nav className="mrx-auto" navbar>
+              <NavItem className="d-flex align-items-center">
+                <NavLink className="font-weight-bold" href="/" />
+              </NavItem>
+
+              <NavItem className="d-flex align-items-center">
+                <NavLink className="font-weight-bold" href="/">
+                  Home
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col className="d-none d-lg-flex justify-content-end">
+            <Form inline>
+              <Input
+                type="search"
+                className="mr-3"
+                placeholder="Search My Articles"
+              />
+              <Button type="submit" color="info" outline>
+                Search
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Navbar>
+  </header>
+);
+
+export default Header;
---
+++ src/Post.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react";
+
+class Post extends Component {
+  state = { post: null };
+
+  render() {
+    return <Fragment />;
+  }
+}
+
+export default Post;
